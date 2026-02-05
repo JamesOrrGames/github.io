dcc91838e06a113727a5faf4ba268c6f08428ab9
JamesOrrRIT <54995791+JamesOrrRIT@users.noreply.github.com>
2026-02-05 00:47:50 -0500

    2/5/26
    Updated Friends of James page with new jokers, added Castle Crashers creations to showcase page

--- src/slideshowControls.js
+++ src/slideshowControls.js
@@ -1,10 +1,11 @@
-let slideIndex = [1,1,1,1,1];
-let slideId = ["slideshow1", "slideshow2", "slideshow3", "slideshow4","slideshow5"]
+let slideIndex = [1,1,1,1,1,1];
+let slideId = ["slideshow1", "slideshow2", "slideshow3", "slideshow4","slideshow5", "slideshow6"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 showSlides(1, 3);
 showSlides(1, 4);
+showSlides(1, 5);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
